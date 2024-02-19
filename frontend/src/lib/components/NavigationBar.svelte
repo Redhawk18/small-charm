d64b5884bb322fa003813eefee6ba8e9f1f9d57b
Carson Range <crange76@gmail.com>
2024-02-18 20:01:16 -0500

    created frontend

---
+++ frontend/src/lib/components/NavigationBar.svelte
@@ -0,0 +1,3 @@
+<header>
+	<a href="/">Small Charm</a>
+</header>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Footer from '$lib/components/Footer.svelte';
+	import NavigationBar from '$lib/components/NavigationBar.svelte';
+</script>
+
+<NavigationBar />
+<main>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+<Footer />
